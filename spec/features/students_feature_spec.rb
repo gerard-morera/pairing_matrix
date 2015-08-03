b5aaf37b160cd2b8d94a96ec1c66785aaa9afa81
TheoLeanse <theoleanse@gmail.com>
2015-08-03 19:26:48 +0100

    tests features: lists student names & past pairing

--- spec/features/students_feature_spec.rb
+++ spec/features/students_feature_spec.rb
@@ -15,4 +15,20 @@ feature 'students' do
     expect(page).to have_content 'Ronald McDonald'
     expect(page).to have_content 'Fred Flintstone'
   end
+
+  scenario 'lists who a student has paired with' do
+    student     = create(:student, name: 'Pink Panther')
+    student_two = create(:student, name: 'George W Bush')
+    create_pair student, student_two
+    visit root_path
+    select "Pink Panther", from: "studentsSelect"
+    # expect(page).to have_content 'George W Bush - yes'
+    within 'ul#paired' do
+      expect(page).to have_content 'George W Bush'
+    end
+  end
+end
+
+def create_pair student_one, student_two
+  create(:pair, student_one: student_one, student_two: student_two)
 end
