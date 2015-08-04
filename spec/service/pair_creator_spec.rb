b19456ca4e449b88015bcf99d9acfe90dbdd9811
Gerard Morera <grdmorera@gmail.com>
2015-08-04 14:45:06 +0100

    list students factory girl stubed

--- spec/service/pair_creator_spec.rb
+++ spec/service/pair_creator_spec.rb
@@ -1,1 +1,18 @@
 require 'rails_helper'
+
+describe PairCreator do
+  let(:students) do
+    [
+      Elin    = create(:student, name: :Elin   ),
+      Gerard  = create(:student, name: :Gerard ),
+      Tolomy  = create(:student, name: :Tolomy ),
+      Faisal  = create(:student, name: :Faisal ),
+      Hitler  = create(:student, name: :Hitler ),
+      Theo    = create(:student, name: :Theo   ),
+      A_cat   = create(:student, name: :A_cat  ),
+      Natalie = create(:student, name: :Natalie),
+      Natalia = create(:student, name: :Natalia),
+      Rob     = create(:student, name: :Rob    )
+    ]
+  end
+end
