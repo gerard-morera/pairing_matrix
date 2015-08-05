e33dac58dea5bffbe0b5b6560885d791ea88e9d1
Gerard Morera <grdmorera@gmail.com>
2015-08-05 15:45:04 +0100

    adding balcklist resource, method new & taking wrong realtions out from model

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,5 +3,4 @@ class Student < ActiveRecord::Base
   has_many :partners, through: :pairs
   has_many :paired_students, through: :pairs, source: :students
   has_many :blacklists
-  has_many :enemies, through: :blacklists, class_name: 'Student'
-end 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: 'pairs#index'
   resources :students do
     resources :partners
+    resources :blacklist, :only => [:new]
   end
-
 end
