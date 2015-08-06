6e7d0cb23d5671f2ddc94db34b7001ca4e68a16d
Gerard Morera <grdmorera@gmail.com>
2015-08-06 11:43:06 +0100

    preparing to change model blacklist to blacklisting

--- app/controllers/blacklist_controller.rb
+++ app/controllers/blacklist_controller.rb
@@ -1,5 +1,6 @@
 class BlacklistController < ApplicationController
   def index
+    # @blacklisted_students = Blacklist.find_by student_id: params[]
   end
 
   def new
---
+++ spec/features/blacklist_feature_spec.rb
@@ -0,0 +1,9 @@
+describe Blacklist do
+  scenario 'viewing my blacklist' do
+    student_1 = create(:student, name: "Mr Burns")
+    student_2 = create(:student, name: "Ms Theo")
+
+    
+
+  end
+end
