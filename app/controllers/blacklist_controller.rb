59e7bdbae85e7d247560c99137c9174a7c0df6ed
Gerard Morera <grdmorera@gmail.com>
2015-08-06 15:49:23 +0100

    update blacklist view

--- app/controllers/blacklist_controller.rb
+++ app/controllers/blacklist_controller.rb
@@ -1,13 +1,19 @@
 class BlacklistController < ApplicationController
   def index
-    @students        = Student.all
-    @current_student = Student.find(params[:student_id])
-    # @blacklists      = Blacklist.
+    @students           = Student.all
+    @current_student    = Student.find(params[:student_id])
+    @potential_partners = @students - @current_student.enemies
   end
 
   def new
   end
 
   def create
+    Blacklist.create(
+      student_id: params[:student_id],
+      enemy_id:   params[:enemy] 
+    )
+    
+    redirect_to student_blacklist_index_path
   end
 end
